9e94d798200b0989eb10ef7e619cb8bbef506c54
more-ka <1640627628@qq.com>
2019-04-12 17:52:46 +0800

    第二种命名空间

--- GetSiblingsAndaddClass.js
+++ GetSiblingsAndaddClass.js
@@ -1,26 +1,29 @@
-let combuDOM = {}
-combuDOM.addClasses = function (node,classes){
-  for(let key in classes){
-    let value = classes[key]
-    let methodName = value?'add':'remove'
-    node.classList[methodName](key)
-  }
-  }
-  
-  //abc为class名字，布尔值控制是否添加
-combuDOM.getSiblings =  function (node) {
-    let allChildren = node.parentNode.children
-    let array = {
-      length: 0
-    }
-    for (let i = 0; i < allChildren.length; i++) {
-      if (allChildren[i] !== node) {
-        array[array.length] = allChildren[i]
-        array.length += 1
+window.node2 = function(node){
+  return {
+    getSiblings:function () {
+      let allChildren = node.parentNode.children
+      let array = {
+        length: 0
       }
-    }
-    return array
+      for (let i = 0; i < allChildren.length; i++) {
+        if (allChildren[i] !== node) {
+          array[array.length] = allChildren[i]
+          array.length += 1
+        }
+      }
+      return array
+    },
+    addClasses: function (classes){
+      for(let key in classes){
+        let value = classes[key]
+        let methodName = value?'add':'remove'
+        node.classList[methodName](key)
+      }
+      }
+      
+      //abc为class名字，布尔值控制是否添加
   }
-
-  combuDOM.addClasses(item2,{a:true,b:true,c:false})
-  console.log(combuDOM.getSiblings(item2))
+}
+var node2 = node2(item3)
+console.log(node2.getSiblings.call(node2))
+node2.addClasses.call(node2,{a:true,b:true,c:false})
