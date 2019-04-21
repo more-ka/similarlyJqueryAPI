f4946fe95ff5a646e67f95227e77f32ac2ec9881
more-ka <1640627628@qq.com>
2019-04-21 17:13:18 +0800

    使用函数封装XMLHttpRequest方法

--- ajax.js
+++ ajax.js
@@ -1,17 +1,31 @@
-button.addEventListener('click',()=>{
+window.jQuery = function(nodeOrSelector){
+  let nodes = []
+  nodes.addClass = function(){}
+  nodes.html = function(){}
+  return nodes
+}
+
+window.jQuery.ajax = function({url,method,body,successFn,failFn}){
   let request = new XMLHttpRequest()
-  request.open('post','http://localhost:8888/xxx')
+  request.open(method,url)
   request.setRequestHeader("Content-Type","application/x-www-form-urlencoded")
-  request.send('请求的第四部分')
+  request.send(body)
   request.onreadystatechange=()=>{
     if(request.readyState === 4){
       console.log('请求响应都结束了')
       if(request.status >= 200 && request.status < 300){
-        console.log('请求成功')
-        console.log(request.status)
+        successFn.call(undefined,request.responseText)
       }else if(request.status >= 400){
-        console.log('请求失败')
-      }
+        failFn.call(undefined,request)
     }
   }
+}}
+button.addEventListener('click',(e)=>{
+  window.jQuery.ajax({
+    url:'http://localhost:8888/xxx',
+    method:'post',
+    body:'这是请求的第四部分',
+    successFn:(responseText)=>{console.log(responseText)},
+    failFn:(request)=>{console.log(request)}
+  })
 })
